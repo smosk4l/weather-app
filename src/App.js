abc087c34fd86e1a481acccb497a043c77a868bf
sm8sk4l <sebastianmoskal@protonmail.com>
2023-03-08 01:49:59 +0100

    connected API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import ImageSearch from "./components/ImageSearch";
+import WeatherCard from "./components/WeatherCard";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [weather, setWeather] = useState([]);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${term}&units=metric&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeather(data);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+  return (
+    <>
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      <WeatherCard />
+    </>
+  );
 }
 
 export default App;
